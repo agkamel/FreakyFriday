5cf5e55465c7cca4c4330230fe5bfb4d8efb1a05
Vincent Gauthier <vincent.d.gauthier@protonmail.com>
2021-03-19 10:02:33 -0400

    Implemented Main window widget.
    Voici nos vrais debut pour PyQt5

--- programmer.py
+++ programmer.py
@@ -11,3 +11,19 @@ from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWid
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QCursor
+
+app = QApplication(sys.argv)
+
+# Set main window and it's properties
+window = QWidget()
+window.setWindowTitle('PyQt5 App')
+window.setGeometry(100, 100, 280, 80)
+window.move(60, 15)
+helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
+helloMsg.move(60, 15)
+
+# Show our window
+
+window.show()
+
+sys.exit(app.exec)

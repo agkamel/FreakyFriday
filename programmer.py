55acb535a2a4df4c1a9b2fb0baa99517b0627bab
Vincent Gauthier <vincent.d.gauthier@protonmail.com>
2021-03-19 12:47:49 -0400

    First implementation of Gui App
    Button now gets time.

--- programmer.py
+++ programmer.py
@@ -11,19 +11,63 @@ from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWid
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QCursor
-
+from datetime import datetime
 app = QApplication(sys.argv)
 
+date_str = "Click the Button!"
+widgets = {
+    "button": [],
+    "message": [],
+    }
+
+
+
 # Set main window and it's properties
+
 window = QWidget()
 window.setWindowTitle('PyQt5 App')
 window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
+
+# Set Layout
+layout = QGridLayout()
+
+
+def clear_widgets():
+    ''' remove all existing widgets from the window
+        & clear the dictionary of widgets'''
+    for widget in widgets:
+        if widgets[widget] != []:
+            widgets[widget][-1].hide()
+            for i in range(0,len(widgets[widget])):
+                widgets[widget].pop()
+
+()
+
+def button_refresh():
+    global date_str
+    clear_widgets()
+    date_str = "Today is:" + str(datetime.now())
+    frame1()
+
+def frame1():
+    '''Define our main frame'''
+
+    button = QPushButton('What is the date?')
+    button.clicked.connect(button_refresh)
+    message = QLabel(date_str)
+
+    widgets["button"].append(button)
+    widgets['message'].append(message)
+    layout.addWidget(widgets["button"][-1], 0, 0)
+    layout.addWidget(widgets["message"][-1], 0, 1)
+    widgets['button'][-1].show()
+    widgets['message'][-1].show()
+
+
 
 # Show our window
-
+window.setLayout(layout)
 window.show()
+frame1()
 
-sys.exit(app.exec)
+sys.exit(app.exec_())

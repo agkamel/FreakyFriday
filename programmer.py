47cadae722ab62d5cc4c533a43e6f539c55624d5
Vincent Gauthier <vincent.d.gauthier@protonmail.com>
2021-03-19 10:12:54 -0400

    Merge cleanup

--- programmer.py
+++ programmer.py
@@ -14,22 +14,16 @@ from PyQt5.QtGui import QCursor
 
 app = QApplication(sys.argv)
 
-<<<<<<< HEAD
 # Set main window and it's properties
-=======
->>>>>>> 01ac434e8b5a195224717cc3f528d7b8ae36a0c6
 window = QWidget()
 window.setWindowTitle('PyQt5 App')
 window.setGeometry(100, 100, 280, 80)
 window.move(60, 15)
 helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
 helloMsg.move(60, 15)
-<<<<<<< HEAD
 
 # Show our window
 
 window.show()
 
 sys.exit(app.exec)
-=======
->>>>>>> 01ac434e8b5a195224717cc3f528d7b8ae36a0c6

76e97dad38385f3eaa0a114260bb018ccae6b160
agkamel <alexandre.gellen.kamel@gmail.com>
2021-03-25 16:17:14 -0400

    Initial commit

---
+++ script/01_import.R
@@ -0,0 +1,36 @@
+# Importing dataset
+
+# This script imports, cleans and exports data
+
+# Packages
+library(readr)
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+
+# Functions
+# s/o
+
+# Settings
+# a. Directory where to save datasets
+data_dir <- "data"
+# b. Path to save datasets
+data_path <- "data/data_fake.rds"
+
+# 1 Importing data
+my_data <- read_rds(data_path)
+
+# 2 Generating graph
+
+# TO BE CONTINUED...
+
+#my_data %>% arrange(button_pushed)
+#  
+
+#  filter(
+#  month(button_pushed) == 3
+#)
+
+
+#ggplot(my_data) +
+#  geom_col(mapping = aes(x = ms(button_pushed), y = ))
